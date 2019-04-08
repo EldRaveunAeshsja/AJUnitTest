754c8de9b5f308f5ad5bdf58e0185ab5d9075d18
Arnaud <everywhere.work@outlook.fr>
2019-04-08 15:38:15 +0200

    camion add capitalist caracteristiques

--- tpGarage/tpGarage/Camion.cs
+++ tpGarage/tpGarage/Camion.cs
@@ -6,5 +6,36 @@ namespace tpGarage
 {
     class Camion : Vehicule
     {
+        // attributs
+        private int _nombreEssieux;
+        private int _poidChargement;
+        private int _volumeChargement;
+
+
+        // accesseurs
+        public int NombreEssieux { get => _nombreEssieux; set => _nombreEssieux = value; }
+        public int PoidChargement { get => _poidChargement; set => _poidChargement = value; }
+        public int VolumeChargement { get => _volumeChargement; set => _volumeChargement = value; }
+
+        // Constructeurs
+        public Camion()
+        {
+            Console.WriteLine("nombre d'essieux: ");
+            _nombreEssieux = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("masse maximale de chargement: ");
+            _poidChargement = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Volume maximale de chargement: ");
+            _volumeChargement = Int32.Parse(Console.ReadLine());
+        }
+
+        public Camion(int nombreEssieux, int poidChargement, int volumeChargement)
+        {
+            _nombreEssieux = nombreEssieux;
+            _poidChargement = poidChargement;
+            _volumeChargement = volumeChargement;
+        }
+
     }
 }
