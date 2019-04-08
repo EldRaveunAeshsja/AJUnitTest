8788a28cb7b123c126ceada8fb8d79ff47722bfa
Arnaud <everywhere.work@outlook.fr>
2019-04-08 15:26:48 +0200

    moto add capitalist caracteristiques

--- tpGarage/tpGarage/Moto.cs
+++ tpGarage/tpGarage/Moto.cs
@@ -6,5 +6,22 @@ namespace tpGarage
 {
     class Moto : Vehicule
     {
+        // attributs 
+        private string _cylindree;
+
+        // accesseur
+        public string Cylindree { get => _cylindree; set => _cylindree = value; }
+
+        // Constructeur
+        public Moto()
+        {
+            Console.WriteLine("Cylindré de la motheu heing");
+            _cylindree = Console.ReadLine();
+        }
+
+        public Moto(string cylindree)
+        {
+            _cylindree = cylindree;
+        }
     }
 }
